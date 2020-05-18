b5cb998353f34bae134c02652d3ea981ca99a119
lauchgott <florianseidel2204@gmail.com>
2020-05-18 15:04:30 +0200

    solution_one

--- src/main/java/de/itdesign/application/XMLCalculator.java
+++ src/main/java/de/itdesign/application/XMLCalculator.java
@@ -1,8 +1,34 @@
-package de.itdesign.application;
+package main.java.de.itdesign.application;
+
+import main.java.de.itdesign.model.input.Data;
+import main.java.de.itdesign.model.input.Operations;
+import main.java.de.itdesign.model.output.Results;
+
+import javax.xml.bind.JAXBException;
+import java.io.File;
+
+import static main.java.de.itdesign.model.output.Results.calculateAllOperations;
+import static main.java.de.itdesign.model.output.Results.createXmlOutput;
 
 public class XMLCalculator {
 
     public static void main(String[] args) {
+
+        try {
+            Data data = new Data();
+            data = data.unmarshalData("data.xml");
+
+            Operations allOperations = new Operations();
+            allOperations = allOperations.unmarshalOperations("operations.xml");
+
+            Results results = new Results();
+            calculateAllOperations(results, data.getCities(), allOperations.getOperations());
+            createXmlOutput(results, new File("outputGenerated.xml"));
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
         //Don't change this part
         if (args.length == 3) {
             //Path to the data file, e.g. data/data.xml
@@ -14,6 +40,9 @@ public class XMLCalculator {
         } else {
             System.exit(1);
         }
+
+
+
 
     }
 
